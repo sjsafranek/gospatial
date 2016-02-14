052016e0ae8adb294a1daef393b2b46dabecdf87
stefan <sjsafranek@gmail.com>
2016-02-13 19:48:18 -0800

    refactor for base find client, and find draw client

--- static/find/L.Find.Draw.js
+++ static/find/L.Find.Draw.js
@@ -11,14 +11,157 @@ L.Find.Draw = L.Class.extend({
     initialize: function(datasources, options) {
         L.setOptions(this, options || {});
         this.find = L.find(datasources);
+        this._featureTypes = {
+            "marker": "Point",
+            "polygon": "Polygon",
+            "rectangle": "Polygon",
+            "polyline": "LineString"
+        };
+        this.drawnItems = null;
         this._map = null;
     },
 
     addTo: function(map) {
         // map.addLayer(this);
-        this.find.addTo(map);
         this._map = map;
+        this.find.addTo(map);
+        this._addMeasureControl();
+        this._addLocateControl();
+        this._addGeosearchControl();
+        this._addDrawingControl();
+        this._addDrawEventHandlers();
+        this._addFeaturePropertiesControl();
+    },
+
+    _addMeasureControl: function() {
+        var measureControl = new L.Control.Measure();
+        measureControl.addTo(this._map);
+    },
+
+    _addLocateControl: function() {
+        L.control.locate().addTo(this._map);
+    },
+
+    _addGeosearchControl: function() {
+        new L.Control.GeoSearch({
+            provider: new L.GeoSearch.Provider.OpenStreetMap()
+        }).addTo(this._map);
+    },
+
+    _addDrawingControl: function() {
+        this.drawnItems = L.featureGroup().addTo(this._map);
+        this._map.addControl(new L.Control.Draw({
+            draw: { circle: false },
+            edit: { featureGroup: this.drawnItems }
+        }));
+    },
+
+    _addFeaturePropertiesControl: function() {
+    	// Ui Control element
+        featurePropertiesControl = L.control({position: 'bottomleft'});
+        featurePropertiesControl.onAdd = function () {
+            var div = L.DomUtil.create('div', 'info legend properties_form');
+            div.innerHTML = "<div>";
+            div.innerHTML += "<strong>Feature Properties </strong>";
+            div.innerHTML += "<a href='#' id='add_property'>[Add Field]</a>";
+            div.innerHTML += "</div>";
+            div.innerHTML += "<div id='properties'>";
+            div.innerHTML += "</div>";
+            return div;
+        };
+        featurePropertiesControl.addTo(this._map);
+        this.preventPropogation(featurePropertiesControl);
+        // Ui Event Handlers
+        $("#add_property").on("click", function() {
+            $("#properties").append("<input type='text' class='field' placeholder='field'><input type='text' class='attr' placeholder='attribute'><br>");
+        });
+    },
+
+    _addDrawEventHandlers: function() {
+        function onMapClick(e) {
+        	var popup = L.popup();
+            if (e.target.editing._enabled) { 
+				console.log('editing enabled')  
+         	}
+            else {
+                popup
+                    .setLatLng(e.latlng)
+                    .setContent("<div class='button' value='Submit Feature' onClick='findDraw.sendFeature(" + e.target._leaflet_id + ")'><h4>Submit Feature</h4><div>")
+                    .openOn(map);
+            }
+        }
+        find_draw = this;
+        this._map.on('draw:created', function(event) {
+            var layer = event.layer;
+            layer.on('click', onMapClick);
+            layer.options.color='blue';
+            layer.layerType = event.layerType;
+            find_draw.drawnItems.addLayer(layer);
+        });
+    },
+
+    preventPropogation: function(obj) {
+    	map = this._map;
+        // http://gis.stackexchange.com/questions/104507/disable-panning-dragging-on-leaflet-map-for-div-within-map
+        // Disable dragging when user's cursor enters the element
+        obj.getContainer().addEventListener('mouseover', function () {
+            map.dragging.disable();
+        });
+        // Re-enable dragging when user's cursor leaves the element
+        obj.getContainer().addEventListener('mouseout', function () {
+            map.dragging.enable();
+        });
+    },
+
+    getProperties: function() {
+        var properties = {};
+        var fields = $("#properties .field");
+        var attrs = $("#properties .attr");
+        for (var _i=0; _i < fields.length; _i++) {
+            properties[fields[_i].value] = attrs[_i].value;
+        }
+        return properties;
+    },
+
+	sendFeature: function(id) {
+        var results;
+        var feature = this.drawnItems._layers[id];
+        var payload = {
+            "geometry": {
+                "type": this._featureTypes[feature.layerType],
+                "coordinates": []
+            },
+            "properties": this.getProperties()
+        }
+        if (payload.geometry.type == "Point") {
+            payload.geometry.coordinates.push(feature._latlng.lng);
+            payload.geometry.coordinates.push(feature._latlng.lat);
+        } else if (payload.geometry.type == "LineString") {
+            for (var i = 0; i < feature._latlngs.length; i++) {
+                payload.geometry.coordinates.push([feature._latlngs[i].lng,feature._latlngs[i].lat])
+            }
+        } else if (payload.geometry.type == "Polygon") {
+            payload.geometry.coordinates.push([]);
+            for (var i = 0; i < feature._latlngs.length; i++) {
+                payload.geometry.coordinates[0].push([feature._latlngs[i].lng,feature._latlngs[i].lat])
+            }
+        } else {
+            alert("Unknown feature type!")
+            return results
+        }
+        results = Utils.postRequest(
+            '/api/v1/layer/' + $('#layers').val() + '/feature',
+            JSON.stringify(payload)
+        );
+        console.log(this.$super);
+        find.getLayer($('#layers').val());
+        map.removeLayer(this.drawnItems._layers[id]);
+        $("#properties .attr").val("");
+        return results;
     }
+
+
+
 
 });
 
--- static/find/find-map.js
+++ static/find/find-map.js
@@ -1,6 +1,7 @@
 
     var map,
         find,
+        findDraw,
         datasources;
 
 /* INITIATE MAP OBJECT */
@@ -15,17 +16,17 @@
         findDraw.addTo(map);
 
     // PREVENT EVENT PROPOGATION TO MAP FOR LEAFLET CONTROL ELEMENTS
-        map.preventPropogation = function(obj) {
-            // http://gis.stackexchange.com/questions/104507/disable-panning-dragging-on-leaflet-map-for-div-within-map
-            // Disable dragging when user's cursor enters the element
-            obj.getContainer().addEventListener('mouseover', function () {
-                map.dragging.disable();
-            });
-            // Re-enable dragging when user's cursor leaves the element
-            obj.getContainer().addEventListener('mouseout', function () {
-                map.dragging.enable();
-            });
-        }
+        // map.preventPropogation = function(obj) {
+        //     // http://gis.stackexchange.com/questions/104507/disable-panning-dragging-on-leaflet-map-for-div-within-map
+        //     // Disable dragging when user's cursor enters the element
+        //     obj.getContainer().addEventListener('mouseover', function () {
+        //         map.dragging.disable();
+        //     });
+        //     // Re-enable dragging when user's cursor leaves the element
+        //     obj.getContainer().addEventListener('mouseout', function () {
+        //         map.dragging.enable();
+        //     });
+        // }
 
 
     // Prepare baselayers
@@ -57,122 +58,121 @@
 
     // Drawing
         map.enableDrawing = function() {
-            if (this.drawing) {
-                return;
-            }
-            this.drawing = {};
-            // Add controls
-            var measureControl = new L.Control.Measure();
-            measureControl.addTo(this);
-            L.control.locate().addTo(this);
-            new L.Control.GeoSearch({
-                provider: new L.GeoSearch.Provider.OpenStreetMap()
-            }).addTo(this);
+            // if (this.drawing) {
+            //     return;
+            // }
+            // this.drawing = {};
+            // // Add controls
+            // var measureControl = new L.Control.Measure();
+            // measureControl.addTo(this);
+            // L.control.locate().addTo(this);
+            // new L.Control.GeoSearch({
+            //     provider: new L.GeoSearch.Provider.OpenStreetMap()
+            // }).addTo(this);
 
             // Draw Features
-            this.drawing.drawnItems = L.featureGroup().addTo(this);
-            this.addControl(new L.Control.Draw({
-                draw: { circle: false },
-                edit: { featureGroup: this.drawing.drawnItems }
-            }));
+            // this.drawing.drawnItems = L.featureGroup().addTo(this);
+            // this.addControl(new L.Control.Draw({
+            //     draw: { circle: false },
+            //     edit: { featureGroup: this.drawing.drawnItems }
+            // }));
 
-            this.drawing._feature_types = {
-                "marker": "Point",
-                "polygon": "Polygon",
-                "rectangle": "Polygon",
-                "polyline": "LineString"
-            };
+            // this.drawing._feature_types = {
+            //     "marker": "Point",
+            //     "polygon": "Polygon",
+            //     "rectangle": "Polygon",
+            //     "polyline": "LineString"
+            // };
 
-            this.drawing.sendFeature = function(id) {
-                var results;
-                var feature = this.drawnItems._layers[id];
-                var payload = {
-                    "geometry": {
-                        "type": this._feature_types[feature.layerType],
-                        "coordinates": []
-                    },
-                    "properties": this._getProperties()
-                }
-                if (payload.geometry.type == "Point") {
-                    payload.geometry.coordinates.push(feature._latlng.lng);
-                    payload.geometry.coordinates.push(feature._latlng.lat);
-                } else if (payload.geometry.type == "LineString") {
-                    for (var i = 0; i < feature._latlngs.length; i++) {
-                        payload.geometry.coordinates.push([feature._latlngs[i].lng,feature._latlngs[i].lat])
-                    }
-                } else if (payload.geometry.type == "Polygon") {
-                    payload.geometry.coordinates.push([]);
-                    for (var i = 0; i < feature._latlngs.length; i++) {
-                        payload.geometry.coordinates[0].push([feature._latlngs[i].lng,feature._latlngs[i].lat])
-                    }
-                } else {
-                    alert("Unknown feature type!")
-                    return results
-                }
-                results = Utils.postRequest(
-                    '/api/v1/layer/' + $('#layers').val() + '/feature',
-                    JSON.stringify(payload)
-                );
-                console.log(this.$super);
-                find.getLayer($('#layers').val());
-                map.removeLayer(this.drawnItems._layers[id]);
-                $("#properties .attr").val("");
-                return results;
-            }
+            // this.drawing.sendFeature = function(id) {
+            //     var results;
+            //     var feature = this.drawnItems._layers[id];
+            //     var payload = {
+            //         "geometry": {
+            //             "type": this._featureTypes[feature.layerType],
+            //             "coordinates": []
+            //         },
+            //         "properties": this.getProperties()
+            //     }
+            //     if (payload.geometry.type == "Point") {
+            //         payload.geometry.coordinates.push(feature._latlng.lng);
+            //         payload.geometry.coordinates.push(feature._latlng.lat);
+            //     } else if (payload.geometry.type == "LineString") {
+            //         for (var i = 0; i < feature._latlngs.length; i++) {
+            //             payload.geometry.coordinates.push([feature._latlngs[i].lng,feature._latlngs[i].lat])
+            //         }
+            //     } else if (payload.geometry.type == "Polygon") {
+            //         payload.geometry.coordinates.push([]);
+            //         for (var i = 0; i < feature._latlngs.length; i++) {
+            //             payload.geometry.coordinates[0].push([feature._latlngs[i].lng,feature._latlngs[i].lat])
+            //         }
+            //     } else {
+            //         alert("Unknown feature type!")
+            //         return results
+            //     }
+            //     results = Utils.postRequest(
+            //         '/api/v1/layer/' + $('#layers').val() + '/feature',
+            //         JSON.stringify(payload)
+            //     );
+            //     console.log(this.$super);
+            //     find.getLayer($('#layers').val());
+            //     map.removeLayer(this.drawnItems._layers[id]);
+            //     $("#properties .attr").val("");
+            //     return results;
+            // }
 
-            var popup = L.popup();
-            function onMapClick(e) {
-                if (e.target.editing._enabled) {  console.log('editing enabled')  }
-                else {
-                    popup
-                        .setLatLng(e.latlng)
-                        .setContent("<div class='button' value='Submit Feature' onClick='map.drawing.sendFeature(" + e.target._leaflet_id + ")'><h4>Submit Feature</h4><div>")
-                        .openOn(map);
-                }
-            }
+            // var popup = L.popup();
+            // function onMapClick(e) {
+            //     if (e.target.editing._enabled) {  console.log('editing enabled')  }
+            //     else {
+            //         popup
+            //             .setLatLng(e.latlng)
+            //             .setContent("<div class='button' value='Submit Feature' onClick='map.drawing.sendFeature(" + e.target._leaflet_id + ")'><h4>Submit Feature</h4><div>")
+            //             .openOn(map);
+            //     }
+            // }
 
-            this.on('draw:created', function(event) {
-                var layer = event.layer;
-                layer.on('click', onMapClick);
-                layer.options.color='blue';
-                layer.layerType = event.layerType;
-                this.drawing.drawnItems.addLayer(layer);
-            });
+            // this.on('draw:created', function(event) {
+            //     var layer = event.layer;
+            //     layer.on('click', onMapClick);
+            //     layer.options.color='blue';
+            //     layer.layerType = event.layerType;
+            //     this.drawing.drawnItems.addLayer(layer);
+            // });
 
 
-            featurePropertiesControl = L.control({position: 'bottomleft'});
-            featurePropertiesControl.onAdd = function () {
-                var div = L.DomUtil.create('div', 'info legend properties_form');
-                div.innerHTML = "<div>";
-                div.innerHTML += "<strong>Feature Properties </strong>";
-                // div.innerHTML += "<a href='#' id='add_property'><i class='fa fa-plus'></i>Add Field</a>";
-                div.innerHTML += "<a href='#' id='add_property'>[Add Field]</a>";
-                div.innerHTML += "</div>";
-                div.innerHTML += "<div id='properties'>";
-                div.innerHTML += "</div>";
-                return div;
-            };
-            featurePropertiesControl.addTo(this);
-            this.preventPropogation(featurePropertiesControl);
+            // featurePropertiesControl = L.control({position: 'bottomleft'});
+            // featurePropertiesControl.onAdd = function () {
+            //     var div = L.DomUtil.create('div', 'info legend properties_form');
+            //     div.innerHTML = "<div>";
+            //     div.innerHTML += "<strong>Feature Properties </strong>";
+            //     div.innerHTML += "<a href='#' id='add_property'>[Add Field]</a>";
+            //     div.innerHTML += "</div>";
+            //     div.innerHTML += "<div id='properties'>";
+            //     div.innerHTML += "</div>";
+            //     return div;
+            // };
+            // featurePropertiesControl.addTo(this);
+            // this.preventPropogation(featurePropertiesControl);
 
-            $("#add_property").on("click", function() {
-                $("#properties").append("<input type='text' class='field' placeholder='field'><input type='text' class='attr' placeholder='attribute'><br>");
-            });
+            // $("#add_property").on("click", function() {
+            //     $("#properties").append("<input type='text' class='field' placeholder='field'><input type='text' class='attr' placeholder='attribute'><br>");
+            // });
 
-            this.drawing._getProperties = function() {
-                var prop = {};
-                var fields = $("#properties .field");
-                var attrs = $("#properties .attr");
-                for (var _i=0; _i < fields.length; _i++) {
-                    prop[fields[_i].value] = attrs[_i].value;
-                }
-                return prop;
-            }
+            // this.drawing._getProperties = function() {
+            //     var prop = {};
+            //     var fields = $("#properties .field");
+            //     var attrs = $("#properties .attr");
+            //     for (var _i=0; _i < fields.length; _i++) {
+            //         prop[fields[_i].value] = attrs[_i].value;
+            //     }
+            //     return prop;
+            // }
         }
 
 
         // map.setView([0,0], 1);
-        map.enableDrawing();
+        // map.enableDrawing();
 
     }
 

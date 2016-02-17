872f7b2737e1da13bc761c24d7334e502364bd72
stefan <sjsafranek@gmail.com>
2016-02-16 23:51:25 -0800

    missing template files

---
+++ static/find/find-map_admin.js
@@ -0,0 +1,37 @@
+
+	var map,
+		FindGeo,
+		datasources;
+
+	function initialize(div, datasources) {
+
+		// map = L.map('map',{maxZoom: 22});
+		map = L.map('map',{maxZoom: 18});
+
+		FindGeo = L.find.draw(datasources);
+		FindGeo.addTo(map);
+
+		osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',{ 
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+		});
+		osm.addTo(map);
+		map._baseMaps = {
+			"OSM": osm,
+			"Topographic": L.tileLayer("http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png",{maxNativeZoom:22}),
+			"Streets": L.tileLayer("http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}.png",{maxNativeZoom:22}),
+			"Imagery": L.tileLayer("http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png",{maxNativeZoom:22})
+		};
+
+		var osmb = new OSMBuildings(map)
+		   .date(new Date(2015, 5, 15, 17, 30))
+		   .load()
+		   .click(function(id) {
+				console.log('feature id clicked:', id);
+		   }
+		);
+
+		var overlayMaps = { Buildings: osmb };
+
+		L.control.layers(map._baseMaps, overlayMaps, {position: 'topright'}).addTo(map);
+
+	}

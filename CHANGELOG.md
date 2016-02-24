7dbc26256df2a87ac9c4aaaf9c599e4252a11009
stefan <sjsafranek@gmail.com>
2016-02-23 23:09:18 -0800

    leaflet.draw with touch

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,12 +3,13 @@
 ## Unreleased
 
 ### Changed
- -
+ - Removed redundant logging from socketHandlers.go
 
 ### Added
  - Version flag (-v)
  - Viewer count to map.html template
  - send json through websocket with viewer count and instructions to update layer
+ - Touch screen support for drawing features (https://github.com/michaelguild13/Leaflet.draw) 
 
 ### Removed
  - 
--- src/gospatial/app/socketHandlers.go
+++ src/gospatial/app/socketHandlers.go
@@ -78,72 +78,8 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 	} else {
 		Hub.Sockets[ds] = make(map[int]*websocket.Conn)
 		Hub.Sockets[ds][conn.c] = ws
-		Info.Println("Socket added")
-		Info.Println(Hub.Sockets[ds])
+		Info.Println("WebSocket connection open")
 	}
 	Hub.broadcast(false, &conn)
 	go messageListener(&conn)
 }
-
-/*
-type connection struct {
-	ws *websocket.Conn
-	ds string
-}
-
-type hub struct {
-	Sockets map[*websocket.Conn]string
-}
-
-func (h hub) broadcast(conn *connection) {
-	Trace.Println("Broadcasting message to open websocket connections")
-	for i := range h.Sockets {
-		if i != conn.ws && h.Sockets[i] == conn.ds {
-			i.WriteMessage(websocket.TextMessage, []byte(`update layer`))
-		}
-	}
-}
-
-var Hub = hub{
-	Sockets: make(map[*websocket.Conn]string),
-}
-
-func messageListener(conn *connection) {
-	defer func() {
-		Debug.Printf("Disconnecting websocket connection")
-		conn.ws.Close()
-		delete(Hub.Sockets, conn.ws)
-	}()
-	for {
-		_, message, err := conn.ws.ReadMessage()
-		if err != nil {
-			Warning.Println(err)
-			break
-		}
-		Debug.Printf("Message: %s %s", string(message), conn.ds)
-		Hub.broadcast(conn)
-	}
-}
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	Trace.Println("Establishing websocket connection")
-	vars := mux.Vars(r)
-	ds := vars["ds"]
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		Info.Println(err)
-		return
-	}
-	conn := connection{ws: ws, ds: ds}
-	Hub.Sockets[ws] = ds
-	go messageListener(&conn)
-}
-*/

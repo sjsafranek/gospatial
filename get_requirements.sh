0734381bc39d1036bc0a1ae52ce6d712a690b78e
Stefan Safranek <stefan@stefan>
2016-03-17 09:30:14 -0700

    get requirements

---
+++ get_requirements.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Setting workspace"
+export GOPATH="`pwd`"
+
+# echo "Setting go version"
+# gvm use go1.5.2
+
+echo "Installing requirements"
+if [ ! -d "`pwd`/src/github.com/gorilla/mux" ]; then
+	go get github.com/gorilla/mux
+fi
+
+if [ ! -d "`pwd`/src/github.com/boltdb/bolt" ]; then
+	go get github.com/boltdb/bolt
+fi
+
+if [ ! -d "`pwd`/src/github.com/gorilla/websocket" ]; then
+	go get github.com/gorilla/websocket
+fi

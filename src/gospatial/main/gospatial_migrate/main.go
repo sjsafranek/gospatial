6221828c78f44fe55da41c8985279fa83dbaea89
stefan <sjsafranek@gmail.com>
2016-03-23 00:09:37 -0700

    migrate tool complete

--- src/gospatial/main/gospatial_migrate/main.go
+++ src/gospatial/main/gospatial_migrate/main.go
@@ -9,7 +9,6 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	// "github.com/boltdb/bolt"
 	"github.com/paulmach/go.geojson"
 	"gospatial/app"
 	"io/ioutil"
@@ -27,24 +26,12 @@ func init() {
 	flag.Parse()
 }
 
-type apikey struct {
-	Apikey      string   `json:"apikey"`
-	Datasources []string `json:"datasources"`
-}
-
 type DumpedDatabase struct {
-	Apikeys map[string]apikey                     `json:"apikeys"`
+	Apikeys map[string]app.Customer               `json:"apikeys"`
 	Layers  map[string]*geojson.FeatureCollection `json:"layers"`
 }
 
 func main() {
-
-	// Open db
-	// db, err := bolt.Open("./"+database+".db", 0600, nil)
-	// if err != nil {
-	// 	app.Error.Fatal(err)
-	// }
-	// defer db.Close()
 
 	// Initiate Database
 	app.DB = app.Database{File: "./" + database + ".db"}
@@ -71,13 +58,21 @@ func main() {
 		}
 		// unmarshal data
 		var data DumpedDatabase
-		data.Apikeys = make(map[string]apikey)
+		data.Apikeys = make(map[string]app.Customer)
 		data.Layers = make(map[string]*geojson.FeatureCollection)
 		err = json.Unmarshal(file, &data)
 		if err != nil {
 			app.Error.Fatal(err)
 		}
-		app.Info.Printf("%v\n", data.Layers)
+		// load api keus
+		for k := range data.Apikeys {
+			app.DB.InsertCustomer(data.Apikeys[k])
+		}
+		// load lauers
+		for k := range data.Layers {
+			app.DB.InsertLayer(k, data.Layers[k])
+		}
+
 	} else {
 		app.Error.Fatal("Unknown option:", option)
 	}

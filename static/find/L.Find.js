49e29d11133d2c5386dfd62ec7f7bc98b292fa28
stefan <stefan@stefan.stefan>
2016-04-23 11:46:43 -0700

    url parser

--- static/find/L.Find.js
+++ static/find/L.Find.js
@@ -35,6 +35,31 @@ L.Find = L.Class.extend({
 		return this;
 	},
 
+	parseURL: function(url) {
+		// http://www.abeautifulsite.net/parsing-urls-in-javascript/
+	    var parser = document.createElement('a'),
+	        searchObject = {},
+	        queries, split, i;
+	    // Let the browser do the work
+	    parser.href = url;
+	    // Convert query string to object
+	    queries = parser.search.replace(/^\?/, '').split('&');
+	    for( i = 0; i < queries.length; i++ ) {
+	        split = queries[i].split('=');
+	        searchObject[split[0]] = split[1];
+	    }
+	    return {
+	        protocol: parser.protocol,
+	        host: parser.host,
+	        hostname: parser.hostname,
+	        port: parser.port,
+	        pathname: parser.pathname,
+	        search: parser.search,
+	        searchObject: searchObject,
+	        hash: parser.hash
+	    };
+	},
+
 	_preventPropogation: function(obj) {
 		map = this._map;
 		// http://gis.stackexchange.com/questions/104507/disable-panning-dragging-on-leaflet-map-for-div-within-map

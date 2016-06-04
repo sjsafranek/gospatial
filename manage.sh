e9bd1803d482b92ba9bd75e691b15926fc27c3b2
stefan <stefan@stefan.stefan>
2016-06-04 10:33:23 -0700

    manage.sh cleanup

--- manage.sh
+++ manage.sh
@@ -5,50 +5,6 @@ export GOPATH="`pwd`"
 key="$1"
 
 case $key in
-    # -i|--install)
-
-    #     # Setup working directory
-    #     echo "creating directories..."
-    #     if [ ! -d "`pwd`/bin" ]; then
-    #         mkdir bin
-    #     fi
-    #     if [ ! -d "`pwd`/pkg" ]; then
-    #         mkdir pkg
-    #     fi
-    #     if [ ! -d "`pwd`/log" ]; then
-    #         mkdir log
-    #     fi
-    #     if [ ! -d "`pwd`/src" ]; then
-    #         mkdir src
-    #         mkdir src/gospatial
-    #     fi
-
-    #     # Move source files
-    #     cp -R gospatial/* src/gospatial/
-
-    #     # Download required libraries
-    #     echo "checking requirements..."
-    #     if [ ! -d "`pwd`/src/github.com/gorilla/mux" ]; then
-    #         echo "installing mux..."
-    #         go get github.com/gorilla/mux
-    #     fi
-
-    #     if [ ! -d "`pwd`/src/github.com/boltdb/bolt" ]; then
-    #         echo "installing bolt..."
-    #         go get github.com/boltdb/bolt
-    #     fi
-
-    #     if [ ! -d "`pwd`/src/github.com/gorilla/websocket" ]; then
-    #         echo "installing websocket..."
-    #         go get github.com/gorilla/websocket
-    #     fi
-
-    #     if [ ! -d "`pwd`/src/github.com/paulmach/go.geojson" ]; then
-    #         echo "installing go.geojson..."
-    #         go get github.com/paulmach/go.geojson
-    #     fi
-    #     echo "done!"
-    # ;;
     -c|--clean)
         echo "cleaning working directory..."
         rm bin/* && rm setup || echo "no binaries found"
@@ -58,14 +14,14 @@ case $key in
         rm src/gospatial/app/*.db || echo "no testing db found"
         echo "done"
     ;;
-    # -d|--destroy)
-    #     echo "destroy woring directory"
-    #     rm -rf bin
-    #     rm -rf src
-    #     rm -rf log
-    #     rm -rf pkg
-    #     echo "done"
-    # ;;
+    -d|--destroy)
+        echo "destroy woring directory"
+        rm -rf bin
+        rm -rf src
+        rm -rf log
+        rm -rf pkg
+        echo "done"
+    ;;
     -t| --test)
         cd src/gospatial/app
         go test -bench=. -test.benchmem
@@ -74,11 +30,7 @@ case $key in
         # python3 api_tester.py        
     ;;
     -h| --help)
-# HELP STUFF
-        echo "-c -t -h -r"
-        # cd ../../..
-        # cd tests
-        # python3 api_tester.py        
+        echo "-c -d -t -h"        
     ;;
     *)
         echo "unknown option"

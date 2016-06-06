31338f5e14cbd66d20a3723df9b18b6501690fbe
Stefan Safranek <stefan@stefan>
2016-06-06 11:41:20 -0700

    goreport models.go

--- gospatial/app/models.go
+++ gospatial/app/models.go
@@ -1,16 +1,12 @@
 package app
 
-/*=======================================*/
 // Customer
-/*=======================================*/
 type Customer struct {
 	Apikey      string   `json:"apikey"`
 	Datasources []string `json:"datasources"`
 }
 
-/*=======================================*/
 // MapData for template
-/*=======================================*/
 type MapData struct {
 	Datasource string
 	Apikey     string
